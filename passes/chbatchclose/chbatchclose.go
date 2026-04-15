b86d1b9e57510240046617e1e4df171430a6af48
Cyril de Catheu <cyril.decatheu@clickhouse.com>
2026-04-15 23:09:32 +0200

    add golangci-lint to makefile

--- passes/chbatchclose/chbatchclose.go
+++ passes/chbatchclose/chbatchclose.go
@@ -64,7 +64,7 @@ func (b *batchUsage) report(varName string, pass *analysis.Pass) {
 		// no usage of rows.Next()
 		return
 	}
-	if !(b.deferredClose || b.returned) {
+	if !b.deferredClose && !b.returned {
 		pass.Reportf(b.assignPos,
 			"clickhouse Batch %s must be closed defensively with defer %s.Close() after successful instantiation",
 			varName, varName)
